5fd955a74a314342b57236ebbd0a1a84a81618ed
LogosInc <duomaxwell7032@gmail.com>
2021-07-29 16:53:08 +0800

    2nd time practice

---
+++ app/src/main/java/com/example/foodyudemy/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.foodyudemy
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "6c6567d4dc9543ba9d98f208f80da83b"
+
+
+    }
+}
---
+++ app/src/main/java/com/example/foodyudemy/data/RemoteDataSource.kt
@@ -0,0 +1,16 @@
+package com.example.foodyudemy.data
+
+import com.example.foodyudemy.data.network.FoodRecipeApi
+import com.example.foodyudemy.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipeApi: FoodRecipeApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipeApi.getRecipes(queries)
+    }
+
+}
