e2559cffa3dd52ec18ffd0e0401b7cd9b8b312cf
LogosInc <duomaxwell7032@gmail.com>
2021-08-04 19:27:11 +0800

    Add bottom sheet - END

--- app/src/main/java/com/example/foodyudemy/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foodyudemy/ui/fragments/recipes/RecipesFragment.kt
@@ -1,30 +1,29 @@
- package com.example.foodyudemy.ui.fragments.recipes
+package com.example.foodyudemy.ui.fragments.recipes
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.ViewModel
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.foodyudemy.Constants.Companion.API_KEY
 import com.example.foodyudemy.R
-import com.example.foodyudemy.viewmodels.MainViewModel
 import com.example.foodyudemy.adapters.RecipesAdapter
 import com.example.foodyudemy.databinding.FragmentRecipesBinding
 import com.example.foodyudemy.util.NetworkResult
 import com.example.foodyudemy.util.observeOnce
+import com.example.foodyudemy.viewmodels.MainViewModel
 import com.example.foodyudemy.viewmodels.RecipesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
- @AndroidEntryPoint
- class RecipesFragment : Fragment() {
+@AndroidEntryPoint
+class RecipesFragment : Fragment() {
 
     private var _binding: FragmentRecipesBinding? = null
     private val binding get() = _binding!!
@@ -33,11 +32,13 @@ import kotlinx.coroutines.launch
     private lateinit var mainViewModel: MainViewModel
     private lateinit var recipesViewModel: RecipesViewModel
 
-     override fun onCreate(savedInstanceState: Bundle?) {
-         super.onCreate(savedInstanceState)
-         mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
-         recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
-     }
+    private val args by navArgs<RecipesFragmentArgs>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -58,31 +59,31 @@ import kotlinx.coroutines.launch
         return binding.root
     }
 
-     private fun setupRecyclerView() {
-         binding.recyclerView.adapter = mAdapter
-         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-         showShimmerEffect()
-     }
+    private fun setupRecyclerView() {
+        binding.recyclerView.adapter = mAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
 
-     private fun readDatabase() {
-         lifecycleScope.launch {
-             mainViewModel.readRecipes.observeOnce(viewLifecycleOwner, { database ->
-                 if (database.isNotEmpty()) {
-                     Log.d("RecipesFragment", "readDatabase called!")
-                     mAdapter.setData(database[0].foodRecipe)
-                     hideShimmerEffect()
-                 } else {
-                     requestApiData()
-                 }
-             })
-         }
-     }
+    private fun readDatabase() {
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observeOnce(viewLifecycleOwner, { database ->
+                if (database.isNotEmpty() && !args.backFromBottomSheet) {
+                    Log.d("RecipesFragment", "readDatabase called!")
+                    mAdapter.setData(database[0].foodRecipe)
+                    hideShimmerEffect()
+                } else {
+                    requestApiData()
+                }
+            })
+        }
+    }
 
-     private fun requestApiData() {
+    private fun requestApiData() {
         Log.d("RecipesFragment", "requestApiData called!")
         mainViewModel.getRecipes(recipesViewModel.applyQueries())
         mainViewModel.recipesResponse.observe(viewLifecycleOwner, { response ->
-            when(response) {
+            when (response) {
                 is NetworkResult.Success -> {
                     hideShimmerEffect()
                     response.data?.let { mAdapter.setData(it) }
--- app/src/main/java/com/example/foodyudemy/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
+++ app/src/main/java/com/example/foodyudemy/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -1,22 +1,99 @@
 package com.example.foodyudemy.ui.fragments.recipes.bottomsheet
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.navigation.fragment.findNavController
+import com.example.foodyudemy.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.foodyudemy.Constants.Companion.DEFAULT_MEAL_TYPE
 import com.example.foodyudemy.R
+import com.example.foodyudemy.databinding.FragmentRecipesBinding
+import com.example.foodyudemy.databinding.RecipesBottomSheetBinding
+import com.example.foodyudemy.viewmodels.RecipesViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import java.util.*
 
 class RecipesBottomSheet : BottomSheetDialogFragment() {
 
+    private var _binding: RecipesBottomSheetBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var recipesViewModel: RecipesViewModel
+
+    private var mealTypeChip = DEFAULT_MEAL_TYPE
+    private var mealTypeChipId = 0
+    private var dietTypeChip = DEFAULT_DIET_TYPE
+    private var dietTypeChipId = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+        _binding = RecipesBottomSheetBinding.inflate(inflater, container, false)
+
+        recipesViewModel.readMealAndDietType.asLiveData().observe(viewLifecycleOwner, { value ->
+            mealTypeChip = value.selectedMealType
+            dietTypeChip = value.selectedDietType
+            updateChip(value.selectedMealTypeId, binding.mealTypeChipGroup)
+            updateChip(value.selectedDietTypeId, binding.dietTypeChipGroup)
+        })
+
+        binding.mealTypeChipGroup.setOnCheckedChangeListener { group, selectedChipId ->
+            val chip = group.findViewById<Chip>(selectedChipId)
+            val selectedMealType = chip.text.toString().lowercase(Locale.getDefault())
+
+            mealTypeChip = selectedMealType
+            mealTypeChipId = selectedChipId
+        }
+
+        binding.dietTypeChipGroup.setOnCheckedChangeListener { group, selectedChipId ->
+            val chip = group.findViewById<Chip>(selectedChipId)
+            val selectedDietType = chip.text.toString().lowercase(Locale.getDefault())
+
+            dietTypeChip = selectedDietType
+            dietTypeChipId = selectedChipId
+        }
+
+        binding.applyBtn.setOnClickListener {
+            recipesViewModel.saveMealAndDietType(
+                mealTypeChip,
+                mealTypeChipId,
+                dietTypeChip,
+                dietTypeChipId
+            )
+            val action = RecipesBottomSheetDirections.actionRecipesBottomSheetToRecipesFragment(true)
+            findNavController().navigate(action)
+        }
+
+
+        return binding.root
+    }
+
+    private fun updateChip(chipId: Int, chipGroup: ChipGroup) {
+        if (chipId != 0) {
+            try {
+                chipGroup.findViewById<Chip>(chipId).isChecked = true
+            } catch (e: Exception) {
+                Log.d("RecipesBottomSheet", e.message.toString())
+            }
+        }
     }
 
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
